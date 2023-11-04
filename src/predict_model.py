5fc6d165e60f1b17f156f0632e77e7ac5def842f
taxe10 <tanchavez@lbl.gov>
2023-11-03 17:15:08 -0700

    mod prediction step to report both prob and second to last layer output

--- src/predict_model.py
+++ src/predict_model.py
@@ -12,6 +12,24 @@ from custom_callbacks import PredictionCustomCallback
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
+
+# Prepare the trained model to return both the probabilities and feature vectors
+class CustomModel(tf.keras.Model):
+    def __init__(self, trained_model):
+        super(CustomModel, self).__init__()
+        self.base_model = tf.keras.Model(
+            inputs = trained_model.input,
+            outputs = trained_model.layers[-2].output)
+        self.last_layers = tf.keras.Model(
+            inputs = trained_model.layers[-2].output,
+            outputs = trained_model.output)
+    
+    def call(self, inputs, training=False):
+        second_to_last = self.base_model(inputs)
+        x = self.last_layers(second_to_last)
+        return x, second_to_last
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_info', help='path to dataframe of filepaths')
@@ -24,9 +42,10 @@ if __name__ == '__main__':
     batch_size = data_parameters.batch_size
     logging.info(tf.test.gpu_device_name())
 
-    # Start prediction process
+    # Load trained model and parameters
     loaded_model = load_model(args.model_dir+'/model.keras')
     target_size = model_list[loaded_model._name]
+    custom_model = CustomModel(loaded_model)    # Modify trained model to return prob and f_vec
 
     # Prepare data generators and create a tf.data pipeline of augmented images
     test_dataset, datasets_uris = get_dataset(args.data_info, shuffle=False)
@@ -38,11 +57,12 @@ if __name__ == '__main__':
     class_num = len(classes)
 
     # Start prediction process    
-    prob = loaded_model.predict(test_generator,
-                                verbose=0,
-                                callbacks=[PredictionCustomCallback(datasets_uris, classes)])
+    prob, f_vec = custom_model.predict(test_generator,
+                                       verbose=0,
+                                       callbacks=[PredictionCustomCallback(datasets_uris, classes)])
 
     df_results = pd.DataFrame(prob, columns=classes)
+    df_results['f_vec'] = f_vec
     df_results.index = datasets_uris
 
     # Create output directory if it does not exist
